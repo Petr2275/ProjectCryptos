22acaf0ab718d98b9ecae67fb1d587cd138187f7
Petr2275 <petrmichalik81@gmail.com>
2025-05-26 10:24:24 +0200

    zapsani tridy cryptocontroller,

--- ProjectCryptos/src/main/java/cz/engeto/ProjectCryptos/CryptoController.java
+++ ProjectCryptos/src/main/java/cz/engeto/ProjectCryptos/CryptoController.java
@@ -1,4 +1,39 @@
 package cz.engeto.ProjectCryptos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cryptos")
 public class CryptoController {
+
+    @Autowired
+    private CryptoService cryptoService;
+
+    @GetMapping
+    public List<Crypto> getAllCryptos(@RequestParam(required = false) String sort) {
+        return cryptoService.getAllCryptos(sort);
+    }
+
+    @GetMapping("/{id}")
+    public Crypto getCryptoById(@PathVariable Integer id) {
+        return cryptoService.getCryptoById(id).orElseThrow(() -> new RuntimeException("Crypto not found"));
+    }
+
+    @PostMapping
+    public void addCrypto(@RequestBody Crypto crypto) {
+        cryptoService.addCrypto(crypto);
+    }
+
+    @PutMapping("/{id}")
+    public void updateCrypto(@PathVariable Integer id, @RequestBody Crypto updatedCrypto) {
+        cryptoService.updateCrypto(id, updatedCrypto);
+    }
+
+    @GetMapping("/portfolio-value")
+    public Double getPortfolioValue() {
+        return cryptoService.getPortfolioValue();
+    }
 }
